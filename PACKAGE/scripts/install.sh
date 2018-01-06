aba8f0d46feab39555ce78c75d41080ab3a7e0e4
Gnadelwartz <kay@rrr.de>
2018-01-06 15:13:41 +0000

    fix iconv, add linebreaks to output

--- PACKAGE/scripts/install.sh
+++ PACKAGE/scripts/install.sh
@@ -17,11 +17,11 @@ echo "download latest webmin release ..."
 # unpack and install
 if [ -f "$webmin.$tarext" ]
 then
-    echo "unpacking webmin ..."
+    echo "unpacking webmin ...<br>"
     /bin/tar -xzf "$webmin.$tarext"
     rm "$webmin.$tarext"
 
-    echo "start installtion of `ls -d webmin*` ..."
+    echo "start installation of `ls -d webmin*` ...<br>"
     cd webmin*
     install_dir=`grep "^root=" ${MINICONF}| sed 's/.*root=//'`
     config_dir=`grep "env_WEBMIN_CONFIG=" ${MINICONF}| sed 's/.*_WEBMIN_CONFIG=//'`
@@ -34,11 +34,12 @@ then
 
     export config_dir atboot nouninstall makeboot nostart
     ./setup.sh $install_dir |  /bin/tee install.log | grep -e "\*" -e "Webmin" -e "ERROR" -e "browser" -e ":10000"
-
     cd ..
-    echo cleanup ...
+    # copy dummy iconv to usr/loca/bin
+	[ ! -f "/usr/local/bin/iconv" ] && cp iconv /usr/local/bin
+    echo "cleanup ...<br>"
     rm -rf webmin*
 else
-   echo "Download of webmin failed!"
+   echo "<p>Download of webmin failed!<p>"
    exit 1
 fi
