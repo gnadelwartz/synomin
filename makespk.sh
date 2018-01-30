9ccaba80481f149600cee4b3c7807f7d98c67ea4
Gnadelwartz <kay@rrr.de>
2018-01-30 22:14:56 +0100

    git add README.MD

--- makespk.sh
+++ makespk.sh
@@ -7,6 +7,12 @@ VER=`cat version`
 
 echo "create webmin SPK version ${VER}"
 
+# update REDAME.MD
+mv README.MD README.MD.bak
+sed 's/download \[webmin-.*/download [webmin-'${VER}'.spk](https:\/\/github.com\/gnadelwartz\/synomin\/raw\/master\/webmin-'${VER}'.spk)/' README.MD.bak >README.MD
+git add README.MD 2>&1 >/dev/null
+git commit -m "update Version $VER" 2>&1 >/dev/null
+
 # chreate package.tgz
 echo "make webmin package.tgz ..."
 cd PACKAGE
@@ -21,10 +27,6 @@ mv INFO INFO.bak
 echo "version=\"${VER}\"" >INFO; sed '/version=/d' INFO.bak >>INFO; rm -f INFO.bak
 tar -cf ../webmin-${VER}.spk *
 cd ..
-
-# update REDAME.MD
-mv README.MD README.MD.bak
-sed 's/download \[webmin-.*/download [webmin-'${VER}'.spk](https:\/\/github.com\/gnadelwartz\/synomin\/raw\/master\/webmin-'${VER}'.spk)/' README.MD.bak >README.MD
 
 # clean up
 echo clean up ...
